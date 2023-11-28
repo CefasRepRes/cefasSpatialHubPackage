9e4e3f8d9f09cb118d1d6fc2694fb3d61df3f9a8
Roi Martinez (Cefas) <roi.martinez@cefas.gov.uk>
2023-11-28 14:42:16 +0000

    corrected the method to get the token

--- R/getAGEToken.R
+++ R/getAGEToken.R
@@ -4,19 +4,23 @@ library(cefasSpatialHub)
 library(tidyverse)
 library(httr)
 
-getAGEToken = function ( ) {
+
+
+getAGEToken = function ( portalUrl) {
 
 
 request_body = list(  username =  "spatial.hub_data_analyst",                                    ## specify conditions of teh data to be extracted
                       password = "D@t@Ana!yst!1234",                                ## specify a list of columns to be return or all "*"
-                      client= 'requestip' ,
+                      client= 'referer' ,
+                      referer = portalUrl,
                       expiration=60 ,
                       f = 'pjson'
 )
 
 
 result = POST(url  = "https://giserver.cefas.co.uk/portal/sharing/rest/generateToken/",
-               body = request_body ) %>% content( as="text") %>% fromJSON ()
+               body = request_body,
+              headers = c('content-type' = 'application/x-www-form-urlencoded')) %>% content( as="text") %>% fromJSON ()
 
 
 return ( result$token )
